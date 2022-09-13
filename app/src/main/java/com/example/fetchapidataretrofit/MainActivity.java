626ec0f8ca4bb72508b239bbabe4e55ee6b1de58
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-09-13 16:13:51 +0530

    Add some component

--- app/src/main/java/com/example/fetchapidataretrofit/MainActivity.java
+++ app/src/main/java/com/example/fetchapidataretrofit/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -18,6 +21,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    TextView comments, albums, photos, todos, user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,46 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        comments = findViewById(R.id.comments);
+        albums = findViewById(R.id.albums);
+        photos = findViewById(R.id.photo);
+        todos = findViewById(R.id.todos);
+        user = findViewById(R.id.user);
+        comments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Comm.class));
+            }
+        });
+
+        albums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Album.class));
+            }
+        });
+
+        photos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Phot.class));
+            }
+        });
+
+        todos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Todoss.class));
+            }
+        });
+
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Userss.class));
+            }
+        });
+
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com/").addConverterFactory(GsonConverterFactory.create()).build();
         JsonPlaceHolder jsonPlaceHolder = retrofit.create(JsonPlaceHolder.class);
@@ -32,13 +77,11 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<ModelClass>>() {
             @Override
             public void onResponse(Call<List<ModelClass>> call, Response<List<ModelClass>> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     List<ModelClass> list = response.body();
-                    AdapterClass modelClass = new AdapterClass(list,MainActivity.this);
+                    AdapterClass modelClass = new AdapterClass(list, MainActivity.this);
                     recyclerView.setAdapter(modelClass);
                 }
-
-
             }
 
             @Override
---
+++ app/src/main/java/com/example/fetchapidataretrofit/Phot.java
@@ -0,0 +1,14 @@
+package com.example.fetchapidataretrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Phot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phot);
+    }
+}
